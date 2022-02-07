dcee54e7e88a892e3a1787e6936a1a928002cc7b
brokenfingers <buvotrys@gmail.com>
2022-02-07 12:02:47 +0200

    finito le koncert

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
-const holder = document.querySelector('.background-holder');
+let holder = document.querySelector('.background-holder');
 const holderWidth = Math.ceil(holder.clientWidth / 10) * 10;
-const holderHeigth = holder.clientHeight;
+const holderHeigth = Math.ceil(holder.clientHeight / 10);
 const rectSize = 10;
+let color = randomColor();
 
-for (let i = 0; i < holderWidth * 20; i += 10) {
+for (let i = 0; i < holderWidth * holderHeigth; i += 10) {
     const newRect = document.createElement('div');
-    newRect.setAttribute('id', i);
+    newRect.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
     newRect.classList.add('rectangle');
     holder.appendChild(newRect);
 }
 
-function wait(ms) {
-    var start = new Date().getTime();
-    var end = start;
-    while (end < start + ms) {
-        end = new Date().getTime();
-    }
+holder.addEventListener("click", onHolderClick);
+
+
+function onHolderClick() {
+    color = randomColor();
+    rectArray = [];
+    rectArray = [...document.querySelectorAll('.rectangle')];
+    changeColor();
 }
 
-const rectArray = [...document.querySelectorAll('.rectangle')];
+function randomColor() {
+    let arr = [];
+    arr[0] = Math.floor(Math.random() * 256);
+    arr[1] = Math.floor(Math.random() * 256);
+    arr[2] = Math.floor(Math.random() * 256);
+    return arr;
+}
 
+let rectArray = [...document.querySelectorAll('.rectangle')];
+console.log(rectArray.length);
 
 function changeColor() {
     setTimeout(() => {
         if (rectArray.length > 0) {
             let randIndex = Math.floor(Math.random() * rectArray.length);
-            rectArray[randIndex].style.backgroundColor = "red";
+            rectArray[randIndex].style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
             rectArray.splice(randIndex, 1);
             changeColor();
         }
-    }, 0.2)
+    }, 4)
 }
 
 changeColor();
